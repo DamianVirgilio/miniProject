19276fbce0e88eb2c256bea865bf943ab4b295ad
DamianVirgilio <damianvirgili@live.nl>
2024-02-23 15:26:28 +0100

    Weapons

--- Weapons.cs
+++ Weapons.cs
@@ -2,9 +2,25 @@ class Weapons
 {
     public string weaponName;
     public int damage;
+    public List<Weapons> WeaponList = new();
 
     public Weapons(string _weaponName, int _damage){
         weaponName = _weaponName;
         damage = _damage;
+    }
+
+    ShowWeapons()
+    {
+        if (WeaponList.Count > 0)
+        {
+            foreach (Weapons weapon in WeaponList)
+            {
+                System.Console.WriteLine(weapon);
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("You have no weapons. ");
+        }
     }
 }
