f8ba1376d8b332490aabddba7495611fd9046634
DamianVirgilio <160112008+DamianVirgilio@users.noreply.github.com>
2024-02-23 14:27:39 +0100

    Player.cs

--- Player.cs
+++ Player.cs
@@ -1,12 +1,23 @@
 
 public class Player
 {
-    public string name;
-    public int health;
-    public int strength;
-    public int level;
-    public Player(string _name, int _health, int _strength, int _level)
+    public string Name;
+    public int MaxHealth;
+    public int Health;
+    public int Strength;
+    public int Level;
+    
+    public Player(string _name, int _strength)
     {
+        Name = _name;
+        MaxHealth = 100;
+        Health = MaxHealth;
+        Strength = _strength;
+        Level = 1;
+    }
 
+    public bool IsAlive()
+    {
+        return HealthPoints > 0;
     }
 }
