d162c82a4a60d13b82cd8eb0591288a08d0be46e
1012556 <mees.breedveld@gmail.com>
2024-02-23 16:51:37 +0100

    Enemy class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿public class Program01
+﻿public class Program
 {
     public void Main()
     {
         do
         {
-            
+            Enemy enemy1 = Enemy.GetEnemy(1);
+            System.Console.WriteLine(enemy1);
+            Enemy enemy2 = Enemy.GetEnemy(2);
+            System.Console.WriteLine(enemy2);
+            Enemy enemy3 = Enemy.GetEnemy(3);
+            System.Console.WriteLine(enemy3);
+            Enemy enemy4 = Enemy.GetEnemy(4);
+            System.Console.WriteLine(enemy4);
+            Enemy enemy5 = Enemy.GetEnemy(5);
+            System.Console.WriteLine(enemy5);
+            break;
         } while (true);
     }
 }
