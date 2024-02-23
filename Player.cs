8458a21ef360f629a3f82f659e852329f8415cdc
DamianVirgilio <damianvirgili@live.nl>
2024-02-23 16:02:46 +0100

    Player

--- Player.cs
+++ Player.cs
@@ -5,15 +5,13 @@ public class Player
     public int MaxHealth;
     public int Health;
     public int Strength;
-    public int Level;
     
-    public Player(string _name, int _strength)
+    public Player(string _name)
     {
         Name = _name;
         MaxHealth = 100;
         Health = MaxHealth;
-        Strength = _strength;
-        Level = 1;
+        Strength = 10;
     }
 
     public bool IsAlive()
@@ -24,5 +22,29 @@ public class Player
     public void TakeDamage(int damage)
     {
         HealthPoints =- damage;
+    }
+
+    public void HealthUpgrade(int AmountHealth)
+    {
+        MaxHealth += AmountHealth;
+        Health = MaxHealth;
+    }
+
+    public void StrengthUpgrade(int AmountStrength)
+    {
+        Strength += AmountStrength;
+    }
+
+    public void HealPlayer(string choice)    // We moeten even bespreken hoe we kiezen uit een healthkit en een bandage
+    {
+        if (choice == ...) // voer string in voor medkit
+        {
+            Health = MaxHealth;
+        }
+        else if (choice == ...) // voer string voor bandage
+        {
+            int healing = MaxHealth / 2;
+            Health += healing;
+        }
     }
 }
