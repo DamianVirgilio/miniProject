7bd8754f7f714d33a1432d6582dbbc0e76d822c8
jurianrandjitsing@gmail.com <jurianrandjitsing@gmail.com>
2024-02-27 02:39:18 -0800

    Program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,215 @@
-﻿public class Program01
+﻿public class Program
 {
     public static void Main()
     {
+        Console.WriteLine
+        (@"
+        
+        You open your eyes and realize that you're in a lift that speeds up and abruptly halts.
+        A group of young men open the lift and one of them shoves you out.
+        You start running but before getting too far you realize that the whole area is walled off.
+
+        They then imprison you and not long after one of the young met free you.
+        His name is Alby.
+
+        Alby asks you who you are, but you have no memory.
+        He then tells you that it's normal since nobody had memory on arrival.
+
+        He further explains that all the young men known as *GLADERS* work together to gether food and build shelters.
+        He also says that the lift where you came from known as *THE BOX* is sent up once a month to deliver anything that they need.
+        It also always carries a new person up but no one ever has any memory.
+
+        Alby introduces you to Newt, his second in command.
+
+        That day you notice an opening in the wall so Alby quickly explains that going beyond the wall is prohibited.
+
+        Later that day, a younger teenager named Chuck comes to help you set up a place to sleep.
+        He also hands you a *WOODEN SPEAR*
+
+        [CHECK WHAT IS AVAILIBLE TO DO IN BASE]
+
+        [WHEN EXITING BASE]
+
+        You and chuck head to the opening in the wall.
+        2 Runners come out of the Wall and Chuck clarifies that one one except runners are allowed past the Walls.
+
+        Newt comes to you and explains that past the Wall lies a Maze. He further says that the Runners have been mapping the maze for 3 years.
+        He further points out that there is a sublte sound that echoes around them noting that it is the mazed that is constantly changing.
+        Only the strongers and fastest Gladers can be Runners because no one has been able to survive being stuck through the night.
+        He then tells you about the mechanical Monsters called Grievers that kill whoever gets stuck in the Maze.
+
+        Later that day you declare that you want to be a runner.
+        As soon as you say that you want to be a runner, one of the Gladers called Gally challenges you to spar.
+
+        [HAND COMBAT, YOU VS GALLY]
+        [HE FIRST DEFEATS YOU AND TAUNTS YOU BUT THEN YOU FIND A WAY TO USE HIS OWN STRENGTH AGAINST HIM]
+        [IMPORTANT; AT THE END OF THE FIGHT YOU FALL AND HIT YOUR HEAD]
+
+        You suddenly remember your NAME!
+
+        [INPUT NAME]
+
+        You scream that your name is [NAME]
+        The Gladers celibrate and welcome you to the group, even Gally.
+
+        The celebration cuts when something wails in the distance.
+        Gally notes that that is a Griever which luckily cannot get past the Walls.
+
+        When you sleep, you get flashes of MEMORY.
+        You see FACES of DOCTORS and a *YOUNG WOMAN*
+
+        In the morning ALBY takes you for a walk. He notes that it wasn't always this peaceful in the Glade.
+        Others died due to FEAR, so they established order to maintain peace.
+        Alby notes that you are very curious unlike the others but even whith that you have to follow the rules.
+
+        Later you help with farming while coming up with ways to escape.
+        Everything you come up with has already been tried and failed.
+
+        You then head to the woods, but one of the Gladers; Ben SUDDENLY appears and JUMPS on you.
+        He LOOKS SICK and JUMPS on you and attemps to CHOKE you!
+
+        [Try to breate]
+        [Find a object near you to SLAM Ben's head to ESCAPE]
+
+        After slamming [OBJECT] on his head you run and try to escape.
+        You RUN back to the Glade and get into a FIGHT with Ben!
+
+        [HAND COMBAT, YOU VS BEN]
+        [YOU GRAB YOUR SPEAR AND THROW IT STRAIGHT INTO BEN'S HEAD]
+
+        All the gladers come and see Ben lying dead on the floor.
+        You explain that Ben just ATTACKED YOU out of nowhere!
+        Newt Comes and lifts up Ben's Shirt showing an infected wound.
+
+        Alby explains that Ben was stung by a Griever causing him to be infected by *THE CHANGING*
+        Albny asks what Ben said to you.
+        You say that Ben said that everything was your fault.
+
+        By nightfall the Gladers dump Ben's body in the Maze.
+
+        That evening you dream about a laboratory and a scientist who assures you that *WCKD* is good!
+        You see the young woman's face again and she promises that everything will change.
+        You wake up and find Minho and Alby heading inside the Maze.
+        You wonder why Alby went in since he's not a runner.
+        Newt assumes that Alby wanted to track if Ben's body got eaten.
+        Newt insists that they all trust Alby since he was the first one to arrive at the Glade.
+        He helped everyone who came after.
+
+        The Gladers wait for them to return near nightfall, but the doors soon start closing.
+        Suddenly they spot Minho carrying an Injured Alby.
+        Everyone SCREAMS for him to cross without Alby but with the doors nearly closed Minho is still far behind.
+
+        You IMPULSIVELY slip inside  barelyg making it to the other side before the doors fully close.
+        The two of you check on Alby who got stung.
+
+        The Grievers start wailing into the distance and the walls start to move.
+        Minho gets up to leave Alby behind but you insist on not leaving him.
+        The two of you traverse the maze while carrying Alby!
+
+        You notice vine on the walls so the two of you hoist Alby to hide him up in the vines.
+
+        [YOU ARE NOW IN THE MAZE AND NEED TO EXPLORE AND SURVIVE]
+        
+        [YOU RUN INTO A GRIEVER]
+        [YOU HAVE 2 OPTIONS OF KILLING A GRIEVER]
+        [1. USE YOUR SPEAR]
+        [2. TACTICAL KILL] [GO THROUGH CLOSING WALLS WHERE YOU BARELY MAKE IT OUT]
+
+        [IN THE MORNING WHEN THE WALLS OPEN YOU HEAD BACK AND FIND A SLINGSHOT]
+
+        The next day the Maze opens and to everyone's surprise they see the two of you carrying Alby back.
+        
+        Minho announces: '[NAME] KILLLED A GRIEVER!'
+
+        All of you return to the Council Hall...
+        Gally Highlights: [NAME] killing a Griever could spell trouble for the others.
+        No one knows what happens when you kill a griever.
+        Minho Announces that [NAME] is now a RUNNER!
+
+        Suddenly they hear something moving outside moving outside:
+        *THE BOX* is being lifted up outside out of the normal schedule!
+
+        When they open it:
+        They see a young Woman with a note in her hand.
+        The Note says: 'SHE'S THE LAST ONE! EVER!'
+
+        Suddenly she WAKES up and GHASPS [NAME] before passing out.
+
+        Newt asks you if you know her but you SAY that you don't.
+        The others get nervous with the idea of the box not returning anymore supplies.
+
+        You decide to gack to the Maze but you're stopped by Minho.
+        You point out that no one has ever captured or killed a griever before.
+        It would be in our best intrest to investigate the body.
+
+        [GO BACK INTO THE MAZE TO THE GRIEVERS BODY]
+
+        You look at the Griever and pull out the Grievers LEG which then comes apart.
+        You find a *CILINDER-LIKE DEVICE* inside of it's leg.
+        The device has a little light which is 'RED', the NUMBER '4' and a LABEl: 'WCKD'.
+
+        You return to the Glade;
+
+        [RETURN TO BASE OR EXPLORE MAZE]
+        [EXPLORE ALL SECTORS (1-4)]
+
+        [CONTINUE STORY AFTER EXPLORING ALL SECTORS]
+
+        Minho and you look at the number on the *CILINDER-LIKE-DEVICE*
+        You take a look at the NUMBER: '4'.
+
+        The maze is devided into 4 SECTORS.
+        That could mean that the Griever probably came from sector 4.
+
+        Finally the girl wakes up but has climbed the watch tower and throws rocks at the Gladers.
+        You get her to stop.
+        You climb up the tower but when you reach the top you find her holding a knife.
+        You calm her down and she reveals that her name is Theresa.
+        You introduce yourself and she claims that she doesn't know you.
+
+        Later you mention to her that she came with *The Note*.
+        The Box hasn't lowered since.
+        She recalls being in the water, being watched over by people and a woman that mentions that *WCKD* is good.
+        You recall seeing the woman that said *WCKD* is good.
+        The two of you wonder why no one else has memories other than their names.
+        Theresa shows you 2 syringes that she found in her pocket with the label *WCKD*.
+
+        You get the idea of injecting Alby that is infected with *THE CHANGING* with one of these syringes.
+        You hold Alby down while theresa INJECTS him with one of the syringes.
+        He immediately loses conciousness.
+
+        The two of them head to *THE PIT*
+        You ask why gally HATES you.
+        He reveales that everything started falling apart as soon as you arrived.
+
+        Chuck comes up to you to talk to you about what it's like to live outside of the walls and if he will ever meet his parents.
+        He hands you a toy that he said he thinks came from his parents.
+        He wants you find his parents and give it to them.
+        You assure Chuck that you will find a way for all of you escape and hand him back his toy.
+        You tell him that he will get the chance to give his toy to his parents himself.
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        ");
+        
         System.Console.WriteLine("What is your Name?");
         string Username = System.Console.ReadLine();
         Player player = new Player(Username, World.Locations[0]);
@@ -25,7 +233,7 @@
             Console.WriteLine("Current location: " + player.CurrentLocation.Name);
             Console.WriteLine(player.CurrentLocation.Compass());
             System.Console.WriteLine("Where do you want to go? (N/E/S/W)");
-            string LocationMove = System.Console.ReadLine().ToUpper();
+            string LocationMove = System.Console.ReadLine();
             player.TryMoveTo(player.CurrentLocation.GetLocationAt(LocationMove));
         };
         Console.WriteLine("You have arrived at the goal!");
