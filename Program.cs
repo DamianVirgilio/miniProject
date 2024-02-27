75f1d6591d7feef8a6176495533dd0b3f47836eb
DamianVirgilio <damianvirgili@live.nl>
2024-02-27 18:16:29 +0100

    Errors

--- Program.cs
+++ Program.cs
@@ -344,15 +344,12 @@ public class Program
             {
                 System.Console.WriteLine("The doors are closed you can't return home (watch out for grievers...)");
             }
-            
+
             if (player.CurrentLocation.GetLocationAt(LocationMove).Name == "Gate 2" && check_grieverfight)
             {
                 player.TryMoveTo(player.CurrentLocation.GetLocationAt(LocationMove));
                 System.Console.WriteLine("You have opened the gate to sector 2. ");
             }
-
-            else if (player.CurrentLocation.GetLocationAt(LocationMove).Sector == "Sector 2" && !check_grieverfight)
-
 
             else if (player.CurrentLocation.GetLocationAt(LocationMove).Name == "Gate 2" && !check_grieverfight)
 
